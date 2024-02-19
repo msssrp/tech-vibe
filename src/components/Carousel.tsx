9fa0d1cde07adbc3549814c1ee598697c5cc8712
Niti <earthza6263@gmail.com>
2024-02-19 21:03:59 +0700

    build carousel by embla

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,9 +1,77 @@
-import React from 'react'
+"use client";
+import React, { useEffect } from "react";
+import useEmblaCarousel from "embla-carousel-react";
+import Autoplay from "embla-carousel-autoplay";
 
+const populararticleslist = [
+  {
+    id: 1,
+    image:
+      "https://images.unsplash.com/photo-1587440871875-191322ee64b0?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 2,
+    image:
+      "https://images.unsplash.com/photo-1587440871875-191322ee64b0?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 3,
+    image:
+      "https://images.unsplash.com/photo-1587440871875-191322ee64b0?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 4,
+    image:
+      "https://images.unsplash.com/photo-1587440871875-191322ee64b0?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 5,
+    image:
+      "https://images.unsplash.com/photo-1587440871875-191322ee64b0?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 6,
+    image:
+      "https://images.unsplash.com/photo-1587440871875-191322ee64b0?q=80&w=2071&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+  {
+    id: 7,
+    image:
+    "https://images.unsplash.com/photo-1573167101669-476636b96cea?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+  },
+];
 const Carousel = () => {
-  return (
-    <div>Carousel</div>
-  )
-}
+  const [emblaRef, emblaApi] = useEmblaCarousel(
+    { loop: true, align: 'center', direction:"ltr"}
+    ,[Autoplay({ delay: 4000 })]
+  );
+  useEffect(() => {
+    if (emblaApi) {
+      console.log(emblaApi.slideNodes()); 
+    }
+  }, [emblaApi]);
 
-export default Carousel
+  return (
+    <div className="embla">
+      <div className="emble__viweport" ref={emblaRef}>
+        <div className="embla__container grid grid-flow-col w-1/3">
+          {populararticleslist.map((populararticles) => {
+            return (
+              <div className="embla__slide" key={populararticles.id}>
+                <div className="embla__slide__img object-cover px-2">
+                  <img
+                    src={populararticles.image}
+                    alt=""
+                    className="rounded-3xl w-full h-64"
+                  />
+                </div>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Carousel;
