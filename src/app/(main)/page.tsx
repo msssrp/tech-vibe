8537521644020da83276919ef3ea6777690c094c
msssrp <siripoomcontact@gmail.com>
2024-02-25 23:36:54 +0700

    finish verify user on first signup

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,9 +1,17 @@
 import getUserSession from "@/libs/actions/getSession";
 import UserPage from "./component/UserPage";
 import HomePage from "./component/HomePage";
+import { getUser } from "@/libs/actions/user/user";
+import { redirect } from "next/navigation";
 export default async function Home() {
   const { data } = await getUserSession();
   if (data.user) {
+    const userId = data.user.id;
+    const userData = await getUser(userId);
+    console.log(userData);
+    if (userData.user_verify == false) {
+      return redirect(`/verify/${userData.user_id}`);
+    }
     return <UserPage />;
   }
 
