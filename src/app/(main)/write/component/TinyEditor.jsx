080bb36e135c141fb0a2eb7d8824433e5fb8463c
msssrp <siripoomcontact@gmail.com>
2024-02-28 12:26:07 +0700

    added tinymce

--- src/app/(main)/write/component/TinyEditor.jsx
+++ src/app/(main)/write/component/TinyEditor.jsx
@@ -1,14 +1,12 @@
 "use client";
 import React, { useRef } from "react";
 import { Editor } from "@tinymce/tinymce-react";
-import { createClient } from "@supabase/supabase-js";
+import { uploadImage } from "@/libs/actions/article";
+import createSupabaseClient from "@/libs/supabase/client";
 import { v4 as uuid } from "uuid";
-
 const TinyEditor = () => {
-  const supabase = createClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-  );
+  const supabase = createSupabaseClient();
+  const uid = uuid();
   const editorRef = useRef(null);
   const log = () => {
     if (editorRef.current) {
@@ -19,18 +17,18 @@ const TinyEditor = () => {
     return new Promise(async (resolve, reject) => {
       const file = blobInfo.blob();
       const fileName = blobInfo.filename();
-      const { data, error } = await supabase.storage
-        .from("image")
-        .upload(uuid(), file);
-      if (data) {
-        const imageUrl = data.path;
+      const { data: uploadData, error } = await supabase.storage
+        .from("Images")
+        .upload(uid, file);
+      if (uploadData) {
+        const imageUrl = data.file.path;
         success(imageUrl);
         resolve(
-          `https://ulpjugqpjzizahxbjstt.supabase.co/storage/v1/object/public/image/${imageUrl}`
+          `https://cqphjwakpkovcvrouaoz.supabase.co/storage/v1/object/public/Images/${uuid}/${imageUrl}`
         );
-      } else {
-        console.log(error);
-        reject(error);
+        console.log(
+          `https://cqphjwakpkovcvrouaoz.supabase.co/storage/v1/object/public/Images/${uuid}/${imageUrl}`
+        );
       }
     });
   };
@@ -52,7 +50,7 @@ const TinyEditor = () => {
             content_style:
               "body { font-family:Helvetica,Arial,sans-serif; font-size:14px }",
             quickbars_selection_toolbar: "bold italic link h1 h2 blockquote",
-            quickbars_insert_toolbar: false,
+            quickbars_insert_toolbar: "image media table hr",
             a11y_advanced_options: true,
             file_picker_types: "image",
             quickbars_image_toolbar: false,
