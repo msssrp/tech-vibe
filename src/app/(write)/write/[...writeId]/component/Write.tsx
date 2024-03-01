d38d13fb18c8a1960747810e7da0bbf80263a4c0
Arpapat2410 <120714445+Arpapat2410@users.noreply.github.com>
2024-03-01 16:41:11 +0700

    updated configuration tinyMce

--- src/app/(write)/write/[...writeId]/component/Write.tsx
+++ src/app/(write)/write/[...writeId]/component/Write.tsx
@@ -24,29 +24,11 @@ const Write: React.FC<WriteProps> = ({ user, writeId }) => {
       <WriteNavbar user={user} />
       <div className="container mx-auto px-32 py-10">
         <div className="flex items-center justify-center mt-10 divide-x">
-          <div className="pr-2">
-            <button>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={0.5}
-                stroke="#9A9A9B"
-                className="w-12 h-12">
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M12 9v6m3-3H9m12 0a9 9 0 1 1-18 0 9 9 0 0 1 18 0Z"
-                />
-              </svg>
-            </button>
-          </div>
-          <div className="w-full pl-4">
-            <input
-              type="text"
+          <div className="w-full pl-16">
+            <textarea
               placeholder="Title"
-              className="input input-lg w-full focus:outline-none focus:border-none px-0 text-4xl font-semibold capitalize"
-            />
+              className="textarea input-lg w-full h-auto  focus:outline-none focus:border-none overflow-hidden px-0 text-4xl font-semibold capitalize resize-none"
+            ></textarea>
           </div>
         </div>
         <div className="mt-8">
