c1cf53b6a0cd8faaf7e349989caaa829cf91c02b
msssrp <siripoomcontact@gmail.com>
2024-03-16 22:24:31 +0700

    fixed page layout

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,6 +1,5 @@
 import MainNavbar from "@/components/main/MainNavbar";
 import Footer from "@/components/main/Footer";
-import UserNavbar from "@/components/main/UserNavbar";
 import getUserSession from "@/libs/actions/user/auth/getSession";
 
 export default async function RootLayout({
@@ -10,12 +9,7 @@ export default async function RootLayout({
 }) {
   const { data } = await getUserSession();
   if (data.user) {
-    return (
-      <div>
-        <UserNavbar />
-        {children}
-      </div>
-    );
+    return <div>{children}</div>;
   }
   return (
     <div>
