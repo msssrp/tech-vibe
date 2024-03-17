60c4ac28afa75adb194f7c82432272e6b751888c
Niti <earthza6263@gmail.com>
2024-03-17 12:30:49 +0700

    edit&add profiletabs and Chart

--- src/app/(profile)/profile/component/Profile.tsx
+++ src/app/(profile)/profile/component/Profile.tsx
@@ -6,6 +6,7 @@ import { FaFacebook } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaGithub } from "react-icons/fa6";
 import Link from "next/link";
+import ProfilTabs from "@/components/ui/profile/ProfilTabs";
 
 const Profile = () => {
   const [itemsPerPage, setItemsPerPage] = useState(4);
@@ -25,160 +26,10 @@ const Profile = () => {
             <h2 className="text-3xl font-medium">
               Heather McLeod in Human Parts
             </h2>
-            <button className="btn btn-md btn-circle btn-ghost hover:bg-white">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-                strokeWidth={1.5}
-                stroke="currentColor"
-                className="w-6 h-6"
-              >
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
-                />
-              </svg>
-            </button>
           </div>
-          <div className="flex justify-between items-center space-x-2 border-b px-2 sticky top-0 bg-base-100 z-10">
-            <div className="menubar text-lg">
-              <button className="btn btn-ghost px-4 text-lg font-normal hover:bg-white text-[#606060] hover:text-black">
-                Home
-              </button>
-              <button className="btn btn-ghost px-4 text-lg font-normal hover:bg-white text-[#606060] hover:text-black">
-                Ariticles
-              </button>
-              <button className="btn btn-ghost px-4 text-lg font-normal hover:bg-white text-[#606060] hover:text-black">
-                Library
-              </button>
-              <button className="btn btn-ghost px-4 text-lg font-normal hover:bg-white text-[#606060] hover:text-black">
-                Statistic
-              </button>
-            </div>
-            <div className="pl-6">
-              <label className="rounded-none border-b flex items-center gap-2">
-                <input type="text" className="grow focus:outline-none" />
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  viewBox="0 0 16 16"
-                  fill="#606060"
-                  className="w-4 h-4 opacity-70"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-              </label>
-            </div>
+          <div className="flex items-center space-x-2 mx-2 sticky top-0 bg-base-100 z-10">
+              <ProfilTabs/>
           </div>
-          <ScrollArea
-            className="pr-11 h-[150vh] overflow-auto"
-            type="never"
-            scrollbarSize={8}
-            offsetScrollbars
-          >
-            <div className="space-y-2 ">
-              {articleslist.map((articleslist, id) => {
-                return (
-                  <div
-                    key={id}
-                    className="card card-side border-b rounded-none items-center "
-                  >
-                    <div className="card-body px-4">
-                      <div className="avatar items-center">
-                        <div className="w-8 rounded-full">
-                          <img src={articleslist.image} />
-                        </div>
-                        <p className="ml-2">{articleslist.author}</p>
-                      </div>
-                      <h2 className="card-title text-2xl mt-3">
-                        {articleslist.title}
-                      </h2>
-                      <p className="line-clamp-2 ">
-                        {articleslist.description}
-                      </p>
-                      <div className="flex justify-between items-center mt-3">
-                        <div className="space-x-1 h-8 overflow-hidden w-full">
-                          {tag.map((tag) => {
-                            return (
-                              <button
-                                key={tag.tag_id}
-                                className={`btn btn-sm badge bg-[#F2F2F2] rounded-full `}
-                              >
-                                <p>{tag.tag_name}</p>
-                              </button>
-                            );
-                          })}
-                        </div>
-                        <div className="flex justify-between items-center w-2/3">
-                          <div>
-                            <p className="text-sm">7 min read</p>
-                          </div>
-                          <div>
-                            <button className="btn btn-sm btn-circle btn-ghost hover:bg-white">
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth={1.5}
-                                stroke="currentColor"
-                                className="w-6 h-6"
-                              >
-                                <path
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                  d="M6.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM12.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0ZM18.75 12a.75.75 0 1 1-1.5 0 .75.75 0 0 1 1.5 0Z"
-                                />
-                              </svg>
-                            </button>
-                            <button className="btn btn-sm btn-circle btn-ghost hover:bg-white">
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth={1.5}
-                                stroke="#FFC107"
-                                className="w-6 h-6"
-                              >
-                                <path
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                  d="M17.593 3.322c1.1.128 1.907 1.077 1.907 2.185V21L12 17.25 4.5 21V5.507c0-1.108.806-2.057 1.907-2.185a48.507 48.507 0 0 1 11.186 0Z"
-                                />
-                              </svg>
-                            </button>
-                            <button className="btn btn-sm btn-circle btn-ghost hover:bg-white">
-                              <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                fill="none"
-                                viewBox="0 0 24 24"
-                                strokeWidth={1.5}
-                                stroke="#699BF7"
-                                className="w-6 h-6"
-                              >
-                                <path
-                                  strokeLinecap="round"
-                                  strokeLinejoin="round"
-                                  d="M7.217 10.907a2.25 2.25 0 1 0 0 2.186m0-2.186c.18.324.283.696.283 1.093s-.103.77-.283 1.093m0-2.186 9.566-5.314m-9.566 7.5 9.566 5.314m0 0a2.25 2.25 0 1 0 3.935 2.186 2.25 2.25 0 0 0-3.935-2.186Zm0-12.814a2.25 2.25 0 1 0 3.933-2.185 2.25 2.25 0 0 0-3.933 2.185Z"
-                                />
-                              </svg>
-                            </button>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                    <div>
-                      <img src={articleslist.image} alt="" className="w-96" />
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-          </ScrollArea>
         </div>
         {/* right */}
         <div className="w-2/6 py-12 pl-11 pr-2 ">
