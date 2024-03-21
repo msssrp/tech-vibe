36d6e7149177d50807d2b6685b89c919e46b686a
msssrp <siripoomcontact@gmail.com>
2024-03-21 15:28:07 +0700

    added nprogress on next/link

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,9 +4,8 @@ import "@/styles/globals.css";
 import "@mantine/core/styles.css";
 import "@mantine/notifications/styles.css";
 import { MantineProvider, createTheme } from "@mantine/core";
-
 export const lora = Lora({ subsets: ["latin"] });
-
+import NextTopLoader from "nextjs-toploader";
 export const metadata: Metadata = {
   title: {
     default: "TechVibe",
@@ -29,7 +28,20 @@ export default async function RootLayout({
   return (
     <html lang="en">
       <body className={lora.className}>
-        <MantineProvider theme={theme}>{children}</MantineProvider>
+        <MantineProvider theme={theme}>
+          <NextTopLoader
+            color="#2299DD"
+            initialPosition={0.08}
+            crawlSpeed={200}
+            height={3}
+            crawl={true}
+            showSpinner={false}
+            easing="ease"
+            speed={200}
+            shadow="0 0 10px #2299DD,0 0 5px #2299DD"
+          />
+          {children}
+        </MantineProvider>
       </body>
     </html>
   );
