61898faef2a95b6346b86f3ac2935e781a1287fd
Niti <earthza6263@gmail.com>
2024-09-17 22:31:22 +0700

    edit css user page

--- src/components/main/AllArticleCard.tsx
+++ src/components/main/AllArticleCard.tsx
@@ -57,7 +57,7 @@ const AllArticleCard: React.FC<AllArticlesProps> = async ({
         <div className="flex flex-col">
           <Link
             href={`/${userWithHyphen}/${articleSlug}`}
-            className="card-title text-xl flex-1"
+            className="card-title md:text-lg xl:text-xl flex-1"
           >
             {article.article_title}
           </Link>
@@ -82,9 +82,9 @@ const AllArticleCard: React.FC<AllArticlesProps> = async ({
                 );
               })}
           </div>
-          <div className="flex justify-between items-center w-2/3">
+          <div className="flex justify-end sm:justify-between items-center w-2/3 space-x-2 sm:space-x-0 lg:space-x-2">
             <div>
-              <p className="text-xs">
+              <p className="text-xs text-nowrap">
                 {timeToRead} {timeToRead <= 1 ? "min " : "mins "}read
               </p>
             </div>
